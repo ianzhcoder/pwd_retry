9ea3b96476e3e910d2a94c1c9a4852260b33240d
Ian Zhang <iancyc417@gmail.com>
2018-11-16 18:14:49 +0800

    update file

--- guesspw.py
+++ guesspw.py
@@ -8,8 +8,9 @@ while time < 4:
         break
     else:
         time = time + 1
-        print ("密碼錯誤,還有", 3-time, "次機會")
-
-        if time == 3:
+        print ("密碼錯誤!")
+        if time < 3:
+            print ("還有", 3-time, "次機會")
+        else:
             print ("猜錯三次,鎖定帳號!")
             break
