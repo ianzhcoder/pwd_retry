1aad8feca0123ae749dde66f6da20e573618f13d
Ian Zhang <iancyc417@gmail.com>
2018-11-16 18:09:43 +0800

    Add file

---
+++ guesspw.py
@@ -0,0 +1,15 @@
+#while True:
+time = 0
+while time < 4:
+    password = 'a123456'
+    user_pw = input("Please enter your password:")
+    if user_pw == password:
+        print("登入成功!")
+        break
+    else:
+        time = time + 1
+        print ("密碼錯誤,還有", 3-time, "次機會")
+
+        if time == 3:
+            print ("猜錯三次,鎖定帳號!")
+            break
